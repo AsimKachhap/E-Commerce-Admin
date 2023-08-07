2cb8b4873d37282021b9f78ea56290b5b651339b
unknown <asim26k@gmail.com>
2023-08-08 00:00:40 +0530

    UPDATE Now logged in user can be stored in DB

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,11 +1,16 @@
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import type { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "../../../../lib/adapters/mongoDB";
 
 export const options: NextAuthOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+
       authorization: {
         params: {
           prompt: "consent",
@@ -15,6 +20,13 @@ export const options: NextAuthOptions = {
       },
     }),
   ],
+  session: {
+    // Yet to fix for session strategy: "database"
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+    updateAge: 24 * 60 * 60,
+  },
+
   pages: {},
 };
 
